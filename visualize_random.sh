568aa2a57aa8c61a9554c084afabf67e0a54164c
N1GH7C4P <kimmo.polojarvi@gmail.com>
2023-01-10 17:38:30 +0200

    added screenshot

--- visualize_random.sh
+++ visualize_random.sh
@@ -1,3 +1,3 @@
-./generator --flow-thousand > ./maps/visualize_me.map
+./generator --flow-ten > ./maps/visualize_me.map
 tail -1 [-b] ./maps/visualize_me.map > ./lem-in_visualizer/required
 sh ./lem-in_visualizer/run_visualizer.sh
