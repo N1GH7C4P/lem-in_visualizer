2c23fd82a878fda790e6738a8dc798e000143831
N1GH7C4P <kimmo.polojarvi@gmail.com>
2023-01-10 16:36:04 +0200

    fix another script path

--- run_visualizer.sh
+++ run_visualizer.sh
@@ -1,2 +1,2 @@
-./lem-in < ./maps/visualize_me.map > ./visualizer/visualize_me.output
-./visualizer/love.app/Contents/MacOS/love ./visualizer
+./lem-in < ./maps/visualize_me.map > ./lem-in_visualizer/visualize_me.output
+./lem-in_visualizer/love.app/Contents/MacOS/love ./lem-in_visualizer
